21fb0c79f415dee422ab7d175a52dc1c1ace248d
mengchaoli <mengchaoli0528@gmail.com>
2025-03-27 23:44:39 -0700

    Refresh .gitignore

---
+++ name_converter.py
@@ -0,0 +1,12 @@
+import re
+
+def convert_title(title):
+    title = title.strip()
+    number, rest = title.split(". ", 1)
+    converted = re.sub(r"[^\w\s\-]", "", rest).replace(" ", "_").lower()
+    return f"{number}_{converted}.py"
+
+# 示例
+title = "3179. Find the N-th Value After K Seconds"
+print(convert_title(title))
+sdf
